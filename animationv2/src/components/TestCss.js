be9fc40317eb41ff214c4b90b2bd787b26640631
LonlehKitteh <krzysgach@o2.pl>
2021-06-17 23:16:33 +0200

    losing mind

--- animationv2/src/components/TestCss.js
+++ animationv2/src/components/TestCss.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { motion } from 'framer-motion'
 import { pageTransition, pageVariants } from '../js/pageAnimation'
-import { Button, Form } from 'react-bootstrap'
-import Question from './Question'
+import { Form } from 'react-bootstrap'
+import Question from './questions/Question.jsx'
 
 export default function TestCss() {
 
@@ -19,29 +19,6 @@ export default function TestCss() {
         >
             <Form>
                 <Question />
-                {/* {
-                    answers.map((el, counter) => {
-                        el = answers[ranNums[counter] - 1]
-                        let ranAnswers = shuffle([0, 1, 2])
-                        return (
-                            <Form.Group key={counter}>
-                                <Form.Label>{`${counter + 1}. ${el.pop()}`}</Form.Label>
-                                {ranAnswers.map((answer, key) => {
-                                    return (
-                                        <Form.Check
-                                            key={`${counter}-${key}`}
-                                            type="radio"
-                                            label={el[answer]}
-                                            name="formHorizontalRadios"
-                                            id={`formHorizontalRadios${key + 1}`}
-                                        />
-                                    )
-                                })}
-                            </Form.Group>
-                        )
-                    })
-                } */}
-                <Button>Sprawdź odpowiedzi</Button>
             </Form>
         </motion.div>
     )
