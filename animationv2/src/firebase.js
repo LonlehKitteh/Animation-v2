e888bdc98fb3da287dbf152a6e81a00d60b92b3e
Christopher <krzysztof02gach@gmail.com>
2022-04-22 23:23:15 +0200

    changes in firebase & security

--- animationv2/src/firebase.js
+++ animationv2/src/firebase.js
@@ -3,27 +3,14 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyApvvzXiwVe_BTGoClnEhukbVVZE9I0JTs",
-    authDomain: "animation-project-cc229.firebaseapp.com",
-    projectId: "animation-project-cc229",
-    storageBucket: "animation-project-cc229.appspot.com",
-    messagingSenderId: "1018777048814",
-    appId: "1:1018777048814:web:d9f2e4a0971abf4f6b05ff"
+    apiKey: "AIzaSyBms8Pg_WZ3o4RbfRm2g-6eKAUMgWjACag",
+    authDomain: "animation-eaacf.firebaseapp.com",
+    projectId: "animation-eaacf",
+    storageBucket: "animation-eaacf.appspot.com",
+    messagingSenderId: "231176049537",
+    appId: "1:231176049537:web:a48ee0cd1459d4c7b60e80",
+    measurementId: "G-EPZ9N7TMYQ"
 })
-
-export const actionCodeSettings = {
-    url: 'https://animation-project-cc229.web.app',
-    handleCodeInApp: true,
-    iOS: {
-        bundleId: 'com.example.ios'
-    },
-    android: {
-        packageName: 'com.example.android',
-        installApp: true,
-        minimumVersion: '12'
-    },
-    dynamicLinkDomain: 'example.page.link'
-};
 
 
 export const auth = firebaseConfig.auth()
