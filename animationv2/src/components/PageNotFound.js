22c5a7790cf8627b91fcce4bfbb4e3ca7faccfb2
Christopher <krzysztof02gach@gmail.com>
2022-05-18 14:44:32 +0200

    404 page finished

--- animationv2/src/components/PageNotFound.js
+++ animationv2/src/components/PageNotFound.js
@@ -1,9 +1,32 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import { pageVariants, pageTransition } from '../js/pageAnimation'
+import { Link } from 'react-router-dom'
 
 export default function PageNotFound() {
     return (
-        <div className="page">
-                Page not found! :c
-        </div>
+        <motion.div layout
+            className="page notFound"
+            initial="initial"
+            animate="in"
+            exit="out"
+            variants={pageVariants}
+            transition={pageTransition}
+        >
+            <div className='main-text'>Page not found</div>
+            <div className='empty'></div>
+            <div className='return'>
+                <span>Please check the url address or use the links below</span>
+                <div>
+                    <Link to='/'>Home</Link> / <Link to='/begin'>Begin</Link> / <Link to="/signup">Sign up</Link>
+                </div>
+            </div>
+            <div className="slices">
+                <div className="slice1"></div>
+                <div className="slice2"></div>
+                <div className="slice3"></div>
+                <div className="slice4"></div>
+            </div>
+        </motion.div >
     )
 }
